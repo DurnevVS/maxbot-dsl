48e179b5dc2699f8b9af822b71df42ebb129cc47
DurnevVS <lewbertsklol@gmail.com>
2026-03-19 13:23:37 +0700

    Initial release

---
+++ handlers/dispatcher.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"context"
+
+	maxbot "github.com/max-messenger/max-bot-api-client-go"
+	"github.com/max-messenger/max-bot-api-client-go/schemes"
+)
+
+type Dispatcher struct {
+	routers []*Router
+}
+
+func NewDispatcher() *Dispatcher {
+	return &Dispatcher{}
+}
+
+func (d *Dispatcher) AddRouter(r *Router) *Dispatcher {
+	d.routers = append(d.routers, r)
+	return d
+}
+
+func (d *Dispatcher) Dispatch(api *maxbot.Api, update schemes.UpdateInterface, ctx context.Context) error {
+	var err error
+	for _, r := range d.routers {
+		rErr := r.Resolve(api, update, ctx)
+		if rErr != nil {
+			err = rErr
+		}
+	}
+	return err
+}
