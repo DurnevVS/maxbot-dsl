29b2020203324d30efca220e13809fc715f7498b
DurnevVS <lewbertsklol@gmail.com>
2026-03-23 15:14:47 +0700

    Renamed handlers -> routers

---
+++ routers/handler.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	"context"
+
+	maxbot "github.com/max-messenger/max-bot-api-client-go"
+	"github.com/max-messenger/max-bot-api-client-go/schemes"
+)
+
+type FilterFunc[T schemes.UpdateInterface] func(update T, ctx context.Context) (bool, error)
+
+type HandlerFunc[T schemes.UpdateInterface] func(api *maxbot.Api, update T, ctx context.Context) error
+
+type Handler[T schemes.UpdateInterface] struct {
+	Filters []FilterFunc[T]
+	Handle  HandlerFunc[T]
+}
+
+func (h *Handler[T]) Run(api *maxbot.Api, update T, ctx context.Context) error {
+	for _, f := range h.Filters {
+		ok, err := f(update, ctx)
+		if err != nil || !ok {
+			return nil
+		}
+	}
+	return h.Handle(api, update, ctx)
+}
